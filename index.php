292f916a9ec9727735fd8fda3e02135eb00930fa
Boris Baryshnikov <razel.dark@gmail.com>
2020-04-30 13:44:58 +0300

    add ip info

--- index.php
+++ index.php
@@ -26,6 +26,19 @@ else
         <h1><font color=red>Congratulations!</font></h1>
         <p>Your AWS Elastic Beanstalk <em>PHP</em> application is now running in the AWS&nbsp;Cloud</p>
         <p>You are running PHP version <?= phpversion() ?></p>
+function get_ip_address(){
+    foreach (array('HTTP_CLIENT_IP', 'HTTP_X_FORWARDED_FOR', 'HTTP_X_FORWARDED', 'HTTP_X_CLUSTER_CLIENT_IP', 'HTTP_FORWARDED_FOR', 'HTTP_FORWARDED', 'REMOTE_ADDR') as $key){
+        if (array_key_exists($key, $_SERVER) === true){
+            foreach (explode(',', $_SERVER[$key]) as $ip){
+                $ip = trim($ip); // just to be safe
+
+                if (filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE) !== false){
+                    return $ip;
+                }
+            }
+        }
+    }
+}
     </section>
 
 
